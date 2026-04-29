2389cb0bdbcc6be5b30e322d0d7da01f4b9a7c5a
Jason Smith <jasonsmith@Jasons-MacBook-Pro.local>
2026-04-29 11:47:45 -0400

    fix: added small content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <Stack bg="bg.emphasized" height="100vh">
       <Box h="100%" bg="bg.subtle" p={2} m={4} borderRadius="md" boxShadow="sm">
-        <Stack bg="bg.info" direction="row" justify="space-around" align="center" p={4}>
+        <Stack bg="bg.info" justify="space-around" align="center" p={4}>
           <Text fontSize="4xl" fontWeight="bold" color="fg.muted">Jason Smith</Text>
+          <Text fontSize="2xl" fontWeight="bold" color="fg.muted">Software Engineer | Budding Full Stack Developer | Creative</Text>
         </Stack>
         <Stack direction="row" justify="space-around" align="center" p={4}><ColorModeButton /></Stack>
       </Box>
